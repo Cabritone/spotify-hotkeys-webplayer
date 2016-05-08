ba0a1821115d5645ccb134c4fe8048e5fd165981
Pedro Cabrita <pedro.cabrita@arbormedia.pt>
2016-05-08 20:37:36 +0100

    version 0.2

--- code/script.js
+++ code/script.js
@@ -1,21 +1,25 @@
 function executeAction(command) {
-  chrome.tabs.query({url: 'https://player.spotify.com/*'}, function(tabs) {
 
-    // Open a new spotify tab if one does not exist yet.
-    if (tabs.length === 0) {
-      chrome.tabs.create({url: 'https://player.spotify.com'});
-    }
+	var callback = function(tabs) {
 
-    var code = "document.getElementById('main').contentDocument.getElementById('" + command + "').click()";
-    
-	// Apply command on all spotify tabs.
-    for (var tab of tabs) {
-      chrome.tabs.executeScript(tab.id, {code: code});
-    }
+		// Open a new spotify tab if one does not exist yet.
+		if (tabs.length === 0) {
+		  chrome.tabs.create({url: 'https://play.spotify.com'});
+		}
 
-    // Unload background page as soon as we're done.
-    window.close();
-  });
+		var code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+
+		// Apply command on all spotify tabs.
+		for (var tab of tabs) {
+		  chrome.tabs.executeScript(tab.id, {code: code});
+		}
+		  
+		// Unload background page as soon as we're done.
+		window.close();
+	};
+
+	chrome.tabs.query({url: 'https://play.spotify.com/*'}, callback);
+	//chrome.tabs.query({url: 'https://player.spotify.com/*'}, callback);
 };
 
 chrome.commands.onCommand.addListener(executeAction);
